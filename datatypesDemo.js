9d9e0139e93a45b33cd979326c60148e0b905806
gdevanshu121 <gdevanshu121@gmail.com>
2025-04-11 08:41:39 +0000

    Added new demo

--- datatypesDemo.js
+++ datatypesDemo.js
@@ -3,3 +3,13 @@ let productDiscount = 1500;
 let productName = "Apple Iphone 11";
 let isAvailable = true;
 
+let minusZero = -0;
+console.log(minusZero);
+
+let x;
+let y = null;
+console.log(x);
+console.log(y);
+
+let bigInt = 666926346923694862396496239649823629864283649n;
+console.log(bigInt);
---
+++ objectDemo.js
@@ -0,0 +1,19 @@
+let product = {
+    name: "Apple iphone 11",
+    price: 150000,
+    discount: 15000,
+    isAvailable: false
+};
+
+
+console.log(product);
+
+
+let socialhandles = {
+    fb: null,
+    insta: 6700,
+    linkedin: 2000,
+    x: 5000,
+    insta:9000,
+};
+console.log(socialhandles);
