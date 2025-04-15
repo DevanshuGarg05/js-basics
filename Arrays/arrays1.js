a054d7e70b3cf09abf8ab5ec82b424f219b1dffe
officialdevanshu05 <officialdevanshu05@gmail.com>
2025-04-15 23:58:18 +0000

    Arrays

---
+++ Arrays/arrays1.js
@@ -0,0 +1,26 @@
+let arr1 = []; // empty array
+
+let arr2 = ["devanshu",true, 10, "garg"];
+
+let arr3 = new Array(10,20,30,40,50,60);
+
+console.log(arr1,arr2);
+
+
+arr3.reverse();
+console.log(arr3);
+
+arr3.pop();
+console.log(arr3);
+
+arr3.push(90);
+console.log(arr3);
+
+
+console.log(arr3.length);
+
+arr3.shift();
+console.log(arr3);
+
+arr3.unshift(1);
+console.log(arr3);
