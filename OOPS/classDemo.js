bde7dc53c581f8d55e7e4bceb23ace93fcf35edf
Devanshu Garg <officialdevanshu05@gmail.com>
2025-06-29 19:21:19 +0530

    Added Code

---
+++ OOPS/classDemo.js
@@ -0,0 +1,35 @@
+class product{
+
+    // name;
+    // price;
+    // category;
+    // description;
+    // rating;
+
+    constructor(productName, productPrice, productCategory, productDescription, productRating){
+        this.name = productName;
+        this.price = productPrice;
+        this.category = productCategory;
+        this.description = productDescription;
+        this.rating = productRating;
+    }
+    addToCart(){
+        console.log("Item added to cart");
+    }
+
+    removeFromCart(){
+        console.log("Item removed from cart");
+    }
+
+    displayProductDetails(){
+        console.log("Displaying product details");
+    }
+
+    buyProduct(){
+        console.log("Product purchased");
+    }
+}
+
+let product1 = new product("Laptop", 1000, "Electronics", "High performance laptop", 4.5);
+
+console.log(product1);
---
+++ OOPS/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+ // name;
+    // price;
+    // category;
+    // description;
+    // rating;
