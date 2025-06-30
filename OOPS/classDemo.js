b4d71eb06cb222b70ca0000ce7142f6f6bcd54a8
Devanshu Garg <officialdevanshu05@gmail.com>
2025-07-01 01:59:56 +0530

    Added Code

--- OOPS/classDemo.js
+++ OOPS/classDemo.js
@@ -1,18 +1,33 @@
 class product{
 
-    // name;
-    // price;
-    // category;
-    // description;
-    // rating;
+    #name;
+    #price;
+    category;
+    description;
+    rating;
 
     constructor(productName, productPrice, productCategory, productDescription, productRating){
-        this.name = productName;
-        this.price = productPrice;
+        this.#name = productName;
+        this.#price = productPrice;
         this.category = productCategory;
         this.description = productDescription;
         this.rating = productRating;
     }
+
+
+    getPrice() {
+        return this.#price;
+    }
+
+    setPrice(p) {
+        if (p > 0) {
+            this.#price = p;
+        } else {
+            console.log("Invalid price");
+        }
+    }
+
+
     addToCart(){
         console.log("Item added to cart");
     }
@@ -22,7 +37,7 @@ class product{
     }
 
     displayProductDetails(){
-        console.log("Displaying product details \n", this);
+        console.log("Displaying product details \n", this.#price);
     }
 
     buyProduct(){
@@ -30,7 +45,12 @@ class product{
     }
 }
 
-let product1 = new product("Laptop", 1000, "Electronics", "High performance laptop", 4.5);
+let iphone = new product("iPhone 14", 999, "Electronics", "Latest iPhone model", 4.5);
 
-console.log(product1);
-product1.displayProductDetails();
+console.log(iphone);
+
+iphone.setPrice(1099);
+
+iphone.displayProductDetails();
+
+console.log("Updated Price: ", iphone.getPrice());
